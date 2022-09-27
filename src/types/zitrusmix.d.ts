d6467286fd45dd6a13f553ce53595ad7d896342a
Christian Schuller <cschuller@servusalps.com>
2022-09-27 21:45:17 +0200

    format

--- src/types/zitrusmix.d.ts
+++ src/types/zitrusmix.d.ts
@@ -1,14 +1,12 @@
 import { TemplateResult } from '../templateResult';
 import { Attribute } from './ast';
-import {html} from './html';
+import { html } from './html';
 
 // export type html = function(strings, ...values): TemplateResult
 
 export interface ZitrusmixOptions {
     app: string | readonly string[];
 }
-
-
 
 export interface ElementContext {
     html: html,
@@ -20,5 +18,5 @@ export interface RouteContext {
     html: html
 }
 
-export type elementCallback = function(ElementContext) : TemplateResult;
-export type routeCallback = function(RouteContext) : TemplateResult;
+export type elementCallback = function(ElementContext): TemplateResult;
+export type routeCallback = function(RouteContext): TemplateResult;
